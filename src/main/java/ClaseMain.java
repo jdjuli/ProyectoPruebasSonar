4cf3c2170782662b0a04dcb8246ba3a232e2613f
jdjuli <julisanf@gmail.com>
2018-04-16 10:07:46 +0200

    Adapted proyect to test AvoidVector rule

--- src/main/java/ClaseMain.java
+++ src/main/java/ClaseMain.java
@@ -1,13 +1,20 @@
+import java.util.Vector;
 
 public class ClaseMain {
 
+	private Vector<String> vect;
+	
 	public ClaseMain() {
-		
+		vect = new Vector<String>();
+		vect.add("This ");
+		vect.add("is ");
+		vect.add("a ");
+		vect.add("test");
 	}
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hola mundo");
+		new ClaseMain();
 
 	}
 
