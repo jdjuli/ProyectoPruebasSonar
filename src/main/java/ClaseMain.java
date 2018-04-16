f328144836506551ec3c903b2889bcc564a54605
jdjuli <julisanf@gmail.com>
2018-04-16 12:36:16 +0200

    Modified code to make it fail when AvoidHashmap rule is applied

--- src/main/java/ClaseMain.java
+++ src/main/java/ClaseMain.java
@@ -1,21 +1,24 @@
+import java.util.Hashtable;
 import java.util.Vector;
 
 public class ClaseMain {
 
 	private Vector<String> vect;
-	
+	private Hashtable<String,Integer> tabla;
+
 	public ClaseMain() {
+
 		vect = new Vector<String>();
 		vect.add("This ");
 		vect.add("is ");
 		vect.add("a ");
 		vect.add("test");
+		
+		tabla = new Hashtable<String,Integer>();
+		tabla.put("hola", 1234);
 	}
 
 	public static void main(String[] args) {
-		
 		new ClaseMain();
-
 	}
-
 }
